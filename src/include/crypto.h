dcaf688dfc8456aae76d3ba8108de08d7392b7e4
sumin-yang224 <kzx1547p@gmail.com>
2022-05-08 04:01:13 -0700

    [fix] 오타수정

--- src/include/crypto.h
+++ src/include/crypto.h
@@ -4,7 +4,6 @@
 #include "macro.h"
 #include "openssl.h"
 
-
 typedef enum _textType{
     PLAINTEXT,
     CIPHER
@@ -15,7 +14,7 @@ int generateSymmetricKey(uint8_t *symmetric_key);
 void encryptSymmetricKey(uint8_t* plainText, uint8_t* cipher, int SIZE, uint8_t* symmetricKey, uint8_t* initialVector);
 void decryptSymmetricKey(uint8_t* cipher, uint8_t* plainText, int SIZE, uint8_t* symmetricKey, uint8_t* initialVector);
 
-RSA* rsaFrompublicKey(const char * publicKey);
+RSA* rsaFromPublicKey(const char * publicKey);
 void rc4(uint8_t *inputKey,uint8_t *outputKey,int keyType);
 
 #endif
